93af011c293a5b9a6a5df268657e966ab5694570
Igor Soroka <stupbird@gmail.com>
2019-03-11 07:00:28 +0200

    added commented example of creating svg in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,16 @@
 console.log("Test js")
-var ci = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+
+// Example of dynamic creation of svg
+// // create svg
+// var svgURI = 'http://www.w3.org/2000/svg';
+// var svg = document.createElementNS( svgURI, 'svg' );
+// // SVG attributes, like viewBox, are camelCased. That threw me for a loop
+// svg.setAttribute( 'viewBox', '0 0 100 100' );
+// // create arrow
+// var path = document.createElementNS( svgURI, 'path' );
+// path.setAttribute( 'd', 'M 50,0 L 60,10 L 20,50 L 60,90 L 50,100 L 0,50 Z' );
+// // add class so it can be styled with CSS
+// path.setAttribute( 'class', 'arrow' );
+// svg.appendChild( path );
+// // add svg to page
+// element.appendChild( svg );
