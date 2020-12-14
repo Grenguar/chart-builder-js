4bde023f42aa14518f8e67c513210fb5d31ebdcb
Igor Soroka <igor@soroka.tech>
2020-12-14 20:44:48 +0200

    added github actions and stack params

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -2,20 +2,22 @@ import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
 
+const deployment = process.env.DEPLOYMENT ?? "dev";
+
 export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const myBucket = new s3.Bucket(this, "chart-builder-demo", {
+    const myBucket = new s3.Bucket(this, `chart-builder-demo-${deployment}`, {
       publicReadAccess: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       websiteIndexDocument: "index.html",
     });
 
-    const deployment = new s3Deployment.BucketDeployment(
+    new s3Deployment.BucketDeployment(
       this,
-      "deployStaticWebsite",
+      `deployStaticWebsite-${deployment}`,
       {
         sources: [s3Deployment.Source.asset("../site")],
         destinationBucket: myBucket,
